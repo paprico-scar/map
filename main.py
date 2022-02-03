5c9e3c6bdf4e922d1b151a77660ab626867059f9
yarik <yaroslav.sotnikov.05@bk.ru>
2022-02-03 15:22:55 +0300

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from PyQt5 import *
+from PyQt5 import uic
 from PyQt5.QtWidgets import *
+import sys
 
 
 class Map(QMainWindow):
     def __init__(self):
         super(Map, self).__init__()
+        self.api_start = 'https://geocode-maps.yandex.ru/1.x'
 
+    def initUI(self):
+        uic.loadUi('widget.ui', self)
+        self.butt.clicked.connect(self.shoe_map)
+
+    def show_map(self):
+        coords = self.coord_x.text() + ',' + self.coord_y.text()
+        spn = self.spn.text()
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Map()
+    ex.show()
+    sys.exit(app.exec_())
