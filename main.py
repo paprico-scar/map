b269fbe5e3e658e19ed1826dcedf7d2d90877290
yarik <yaroslav.sotnikov.05@bk.ru>
2022-02-05 14:56:47 +0300

    2ex done

--- main.py
+++ main.py
@@ -27,10 +27,20 @@ class Map(QMainWindow):
         self.lll = ''
         self.butt.clicked.connect(self.first_show_map)
         self.up.clicked.connect(self.do_up)
+        self.down.clicked.connect(self.do_down)
 
     def first_show_map(self):
-        self.spnn = ['spn', self.spn.text]
-        self.lll = ['ll', self.coord_x.text + ',' + self.coord_y.text]
+        self.spnn = ['spn', self.spn.text()]
+        self.lll = ['ll', self.coord_x.text() + ',' + self.coord_y.text()]
+        if self.spnn[1] == '':
+            self.spnn = ['spn', '1,1']
+        my_map = zapros(self.lll, self.spnn)
+        im = Image.open(BytesIO(my_map.content))
+        im.save('map.png')
+        pixmap = QPixmap('map.png')
+        self.map.setPixmap(pixmap)
+
+    def show_map(self):
         if self.spnn[1] == '':
             self.spnn = ['spn', '1,1']
         my_map = zapros(self.lll, self.spnn)
@@ -41,9 +51,19 @@ class Map(QMainWindow):
 
     def do_up(self):
         new_spnn = [float(i) for i in self.spnn[1].split(',')]
-        new_spnn = [str(i - 0.1) for i in new_spnn]
-        new_spnn = ','.join(new_spnn)
-        self.spnn = ['spn', new_spnn]
+        if new_spnn[1] - 0.05 > 0:
+            new_spnn = [str(i - 0.05) for i in new_spnn]
+            new_spnn = ','.join(new_spnn)
+            self.spnn = ['spn', new_spnn]
+        self.show_map()
+
+    def do_down(self):
+        new_spnn = [float(i) for i in self.spnn[1].split(',')]
+        if new_spnn[1] + 0.05 > 0:
+            new_spnn = [str(i + 0.05) for i in new_spnn]
+            new_spnn = ','.join(new_spnn)
+            self.spnn = ['spn', new_spnn]
+        self.show_map()
 
 
 if __name__ == '__main__':
